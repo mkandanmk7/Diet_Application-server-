ccc25fe40f2b7d3a95df666389b7bc37e9accf11
muthu <mkandanmk7@gmail.com>
2021-11-21 13:13:34 +0530

    schema

---
+++ Services/SendMail.js
@@ -0,0 +1,30 @@
+const nodemailer = require("nodemailer");
+
+const sendMail = async (email, subject, link) => {
+  try {
+    let transporter = await nodemailer.createTransport({
+      service: "hotmail",
+      auth: {
+        user: process.env.MAILER_USERNAME,
+        pass: process.env.MAILER_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: "muthutest789@gmail.com",
+      to: email,
+      subject: subject,
+      html: link,
+    };
+
+    transporter.sendMail(mailOptions, (err) => {
+      if (err) {
+        console.log("email error" + err);
+      }
+    });
+  } catch (err) {
+    console.log("error in email sending", err);
+  }
+};
+
+module.exports = sendMail;
